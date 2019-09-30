5e52af0791622a8a199472133aa634300ca6abc3
John Ross <41411318+nitdip@users.noreply.github.com>
2019-09-30 05:49:03 -0700

    initial commit

---
+++ Butter/Butter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 08:46:12 2019
+
+@author: jvalderama
+"""
+import os
+
+os.chdir('..')
+
+path = os.curdir
+
+from USDA import GATS, QuickStats
+#
+product = 'DryWhey'
+
+import_file = f'imports.csv'
+import_codes = [404105010,
+404105090,
+404109000]
+
+gats_imports = GATS(import_file, kind='imports', codes=import_codes, product=product)
+imports = gats_imports.transform()
+gats_imports.save(f'{path}/{product}/{product}Imports')
+
+export_file = f'exports.csv'
+export_codes = [404104000]
+
+gats_exports = GATS(export_file, kind='exports', codes=export_codes, product=product)
+exports = gats_exports.transform()
+gats_exports.save(f'{path}/{product}/{product}Exports')
+
+items = ['HUMAN', 'ANIMAL']
+stock_file = f'{path}/{product}/stock.csv'
+qs_stocks = QuickStats(stock_file, kind='stocks', items=items)
+qs_stocks.save(f'{path}/{product}/{product}Stocks')
+
+data_items = ['Human', 'Animal']
+prod_file = f'{path}/{product}/production.csv'
+qs_production = QuickStats(prod_file, kind='production', items=data_items)
+qs_production.save(f'{path}/{product}/{product}Production')
